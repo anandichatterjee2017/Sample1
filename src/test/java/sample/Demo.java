164edd421c21613e2f868b17e72c42420971273e
Arunava <chat.bunty@gmail.com>
2020-12-18 13:39:51 +0530

    Implement TestNG

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -14,28 +14,27 @@ public class Demo {
 	@Test
 	public void register() {
 		try {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\BrowserDriversEXE\\chromedriver.exe"); 	
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\BrowserDriversEXE\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();
 			driver.get("https://demoqa.com/register");
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-			
+
 			driver.findElement(By.id("firstname")).sendKeys("Arunava");
 			driver.findElement(By.id("lastname")).sendKeys("Chatterjee");
-			
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");  
-			LocalDateTime now = LocalDateTime.now();  
+
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+			LocalDateTime now = LocalDateTime.now();
 			System.out.println(dtf.format(now));
-			
+
 			driver.findElement(By.id("userName")).sendKeys(dtf.format(now));
 			driver.findElement(By.id("password")).sendKeys("Pasword1$");
-			
+
 			driver.quit();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		
+
 	}
 }
